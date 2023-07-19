d4fab189b1c7992fd93007d94f5e43c1c315472a
lenovo <lenovo@DESKTOP-FS73FSD>
2023-07-19 19:34:29 +0530

    2ndcommit

--- Scripts/Spy web/Script1689773762898.groovy
+++ Scripts/Spy web/Script1689773762898.groovy
@@ -17,3 +17,11 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+WebUI.setText(findTestObject('Spy web/Page_OrangeHRM/input_Username_username'), 'Admin')
+
+WebUI.setText(findTestObject('Spy web/Page_OrangeHRM/input_Password_password'), 'admin123')
+
+WebUI.click(findTestObject('Spy web/Page_OrangeHRM/button_Login'))
+
--- Test Suites/Spy web_Test Suite.ts
+++ Test Suites/Spy web_Test Suite.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>c9e1062d-a5a8-434a-91b4-ef12a3679693</testSuiteGuid>
+   <testCaseLink>
+      <guid>5dd3f132-d0d4-4c06-9bd8-0260bfb80e0a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Spy web</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
