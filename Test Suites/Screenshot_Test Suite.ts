a15771fc140859d9b08243072d961428e578ecbd
lenovo <lenovo@DESKTOP-FS73FSD>
2023-07-24 11:34:04 +0530

    6th commit

---
+++ Test Suites/Screenshot_Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Screenshot_Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b7cb6f93-4bb9-41f5-aed8-909a4aeb2714</testSuiteGuid>
+</TestSuiteEntity>
