083c7c77e1ec49de4c58f5b2ccadff2aa6679399
lenovo <lenovo@DESKTOP-FS73FSD>
2023-07-25 15:03:38 +0530

    7 commit

---
+++ Test Suites/Facebook_Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Facebook_Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f70daf8f-b1dd-4ffb-b4db-0db92c159246</testSuiteGuid>
+</TestSuiteEntity>
--- Test Suites/You tube_Test Suite.ts
+++ Test Suites/You tube_Test Suite.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>40cf07bf-7505-4e32-beb3-10b3893b3f19</testSuiteGuid>
+   <testCaseLink>
+      <guid>da3bbfb9-ce87-4451-8728-44ef242afd31</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/You tube</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
