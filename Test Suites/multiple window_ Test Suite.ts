deded48a96f4a2f79c2c55fbdd1327a33f736ea9
lenovo <lenovo@DESKTOP-FS73FSD>
2023-07-25 20:02:51 +0530

    9 commit

---
+++ Test Suites/multiple window_ Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>multiple window_ Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b8bbd9ff-eb93-45d9-b389-c966137b1f69</testSuiteGuid>
+</TestSuiteEntity>
