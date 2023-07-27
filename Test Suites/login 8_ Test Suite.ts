c65b3fdb620dbee99ee703566d04375a55d4916a
lenovo <lenovo@DESKTOP-FS73FSD>
2023-07-27 23:22:54 +0530

    12 commit

--- Test Suites/login 8_ Test Suite.ts
+++ Test Suites/login 8_ Test Suite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>login 7 Test Suite</name>
+   <name>login 8_ Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,12 +10,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>7d1b990a-c9c9-421b-b2cc-2b7b7ac52bed</testSuiteGuid>
+   <testSuiteGuid>16554858-599e-4e71-a3f3-378e29533ee9</testSuiteGuid>
    <testCaseLink>
-      <guid>58849154-f9d8-4060-9679-8026729654ae</guid>
+      <guid>92a7e733-4543-43df-9820-7c0d19e35b6b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/login 7</testCaseId>
+      <testCaseId>Test Cases/login 8</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
